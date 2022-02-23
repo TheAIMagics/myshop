ce5fa7f4a3858db0c028b9c18abd7566c7d18d66
satyazmx <thakursatya6@gmail.com>
2022-02-23 07:53:19 +0530

    Update cart items

--- shop/carts/carts.py
+++ shop/carts/carts.py
@@ -1,26 +1,28 @@
-from flask import render_template, redirect, request, url_for,flash, session, current_app
+from flask import render_template, redirect, request, url_for, flash, session, current_app
 from shop import db, app, photos
 from shop.products.models import Addproduct
 
+
 def MergeDicts(dict1, dict2):
-    if isinstance(dict1,list) and isinstance(dict2, list):
+    if isinstance(dict1, list) and isinstance(dict2, list):
         return dict1 + dict2
     elif isinstance(dict1, dict) and isinstance(dict2, dict):
         return dict(list(dict1.items()) + list(dict2.items()))
     else:
         return False
 
-@app.route('/addcart', methods=['POST','GET'])
+
+@app.route('/addcart', methods=['POST', 'GET'])
 def AddCart():
     try:
         product_id = request.form.get('product_id')
-        quantity =  int(request.form.get('quantity'))
-        colors = request.form.get('colors')
-        product = Addproduct.query.filter_by(id= product_id).first()
+        quantity = int(request.form.get('quantity'))
+        color = request.form.get('colors')
+        product = Addproduct.query.filter_by(id=product_id).first()
 
-        if product_id and quantity and colors and product and request.method =='POST':
+        if product_id and quantity and color and product and request.method == 'POST':
             DictItems = {product_id: {'name': product.name, 'price': float(product.price), 'discount': product.discount,
-                                      'color': colors, 'quantity': quantity, 'image': product.image_1,
+                                      'color': color, 'quantity': quantity, 'image': product.image_1,
                                       'colors': product.colors}}
             if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
@@ -41,16 +43,41 @@ def AddCart():
     finally:
         return redirect(request.referrer)
 
-@app.route('/carts', methods=['POST','GET'])
+
+@app.route('/carts', methods=['POST', 'GET'])
 def getCart():
     if 'Shoppingcart' not in session:
         return redirect(request.referrer)
     subtotal = 0
     grandtotal = 0
     for key, product in session['Shoppingcart'].items():
-        discount = (product['discount']/100)*float(product['price'])
+        discount = (product['discount'] / 100) * float(product['price'])
         subtotal += float(product['price']) * int(product['quantity'])
         subtotal -= discount
         tax = ("%.2f" % (.06 * float(subtotal)))
         grandtotal = float("%.2f" % (1.06 * subtotal))
-    return render_template('products/carts.html', tax=tax, grandtotal=grandtotal)
+    return render_template('products/carts.html', tax=tax, grandtotal=grandtotal, subtotal=subtotal)
+
+
+@app.route('/updatecart/<int:code>', methods=['POST', 'GET'])
+def updatecart(code):
+    if 'Shoppingcart' not in session or len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key, item in session['Shoppingcart'].items():
+                if int(key) == code:
+                    print(type(item['quantity']))
+                    print(type(item['price']))
+                    print("item=", item)
+                    item['quantity'] = quantity
+                    item['color'] = color
+                    flash("Item is updated", 'success')
+                    return redirect(url_for('getCart'))
+
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
